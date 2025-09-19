6ed41a564f39b0fe5e41e508718a83603739e35d
pushsontakke <pusontakke1@gmail.com>
2025-09-20 01:37:14 +0530

    Initial Project Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Pixisphere",
+  description:
+    "A platform that connects customers with the best photographers and studios for maternity, newborn, birthday, and other special shoots.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
